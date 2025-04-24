21cc1983ba109a8db9c63526c2cba2f6a5bfd411
Nnamdi <nnamdi.okafor@stanbicibtc.com>
2025-04-24 15:01:56 +0100

    Add README.md

--- CurrencyConverter.Api/Controllers/WeatherForecastController.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace CurrencyConverter.Api.Controllers;
-
-[ApiController]
-[Route("[controller]")]
-public class WeatherForecastController : ControllerBase
-{
-}
